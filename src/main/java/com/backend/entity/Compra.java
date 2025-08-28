c85c512818144152c237a27d62053e61bd15fd95
DEN0305 <zac5346@gmail.com>
2025-08-28 15:16:32 -0500

    Repositorios e entidades creadas

---
+++ src/main/java/com/backend/entity/Compra.java
@@ -0,0 +1,32 @@
+package com.backend.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "compra")
+@Data
+public class Compra {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotNull(message = "La fecha es obligatoria")
+	private LocalDate fecha; 
+
+	private Integer proveedor;
+
+	@NotNull(message = "El valor es obligatorio")
+	private Integer valor;
+}
---
+++ src/main/java/com/backend/repository/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.entity.Venta;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+
+}
